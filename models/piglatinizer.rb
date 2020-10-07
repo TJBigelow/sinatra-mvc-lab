a6aa68402441094e6d9e5fc72a2685ac6b4894bb
tjbigelow <tjbigelow@att.net>
2020-10-07 14:18:56 -0500

    Done.

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -16,7 +16,7 @@ class PigLatinizer
         ['a','e','i','o','u'].include?(letter.downcase)
     end
     def latin(word)
-        first_vowel = word.split('').find_index {|letter| vowel?(letter)}
+        first_vowel = (word.split('').find_index {|letter| vowel?(letter)} || 0)
         word[first_vowel .. -1] + word[0 ... first_vowel] + (first_vowel == 0 ? 'way' : 'ay')
     end
 end
